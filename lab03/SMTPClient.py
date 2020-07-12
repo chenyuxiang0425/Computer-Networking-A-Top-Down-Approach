5c034940bb798b314b4733eca9fe71f48dbf2475
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-07-12 21:29:29 +0800

    connect to mail server

---
+++ lab03/SMTPClient.py
@@ -0,0 +1,51 @@
+from socket import *
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = "smtp.163.com"
+# Create socket called clientSocket and establish a TCP connection with mailserver
+#Fill in start
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver,25))
+#Fill in end
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+    print('220 reply not received from server.')
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send MAIL FROM command and print server response.
+# Fill in start
+
+# Fill in end
+
+# Send RCPT TO command and print server response.
+# Fill in start
+
+# Fill in end
+
+# Send DATA command and print server response.
+# Fill in start
+
+# Fill in end
+
+# Send message data.
+# Fill in start
+
+# Fill in end
+
+# Message ends with a single period.
+# Fill in start
+
+# Fill in end
+
+# Send QUIT command and get server response.
+# Fill in start
+
+# Fill in end
